da81df44c91eaf7765d5fcdc651329a85bcda5a1
Timothy Cohen <timothya.cohen@yahoo.com>
2013-09-30 19:53:14 -0400

    Heroku setup

---
+++ Werewolf/src/main/java/edu/wm/service/LatLng.java
@@ -0,0 +1,28 @@
+package edu.wm.service;
+
+public class LatLng {
+	
+	private double distance(double lat1, double lon1, double lat2, double lon2, char unit) {
+		  double theta = lon1 - lon2;
+		  double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+		  dist = Math.acos(dist);
+		  dist = rad2deg(dist);
+		  dist = dist * 60 * 1.1515;
+		  return (dist);
+		}
+
+		/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+		/*::  This function converts decimal degrees to radians             :*/
+		/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+		private double deg2rad(double deg) {
+		  return (deg * Math.PI / 180.0);
+		}
+
+		/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+		/*::  This function converts radians to decimal degrees             :*/
+		/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+		private double rad2deg(double rad) {
+		  return (rad * 180.0 / Math.PI);
+		}
+
+}
